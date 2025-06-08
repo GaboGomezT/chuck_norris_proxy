0cd74b0c2c83951466252306c96bdd70c626b0ab
GaboGomezT <gomezgabriel09@gmail.com>
2025-06-07 23:09:45 -0600

    Add ApiProxy.Router module with basic routing for jokes and error handling

---
+++ lib/api_proxy/router.ex
@@ -0,0 +1,14 @@
+defmodule ApiProxy.Router do
+  use Plug.Router
+
+  plug(:match)
+  plug(:dispatch)
+
+  get "/joke" do
+    send_resp(conn, 200, "Here's a Chuck Norris joke.")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
